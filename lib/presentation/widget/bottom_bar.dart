346f750960c4893c172c93d3fb74658635615df5
Bagus Merta <merta.bagus24@gmail.com>
2023-12-29 00:02:14 +0800

    feat: add bottom bar with other child widget

---
+++ lib/presentation/widget/bottom_bar.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:todoapp/presentation/themes/app_theme.dart';
+
+class MyBottomBar {
+  static Widget BottomBarr({required VoidCallback? onTap}) {
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 24),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: Helper.getShadow(
+            color: AppTheme.greyPrimary,
+            blurRadius: 15,
+            offset: const Offset(0, -4),
+          ),
+        ),
+        child: MyPrimaryButton(
+          onTap: onTap,
+          text: 'Add new To Do',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widget/button.dart
@@ -0,0 +1,39 @@
+class MyPrimaryButton extends StatelessWidget {
+  final Function()? onTap;
+  final String? text;
+  final bool isSecondary;
+
+  MyPrimaryButton({
+    Key? key,
+    this.onTap,
+    this.text,
+    this.isSecondary = false, // Default value is false for primary button
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: isSecondary ? Colors.white : AppTheme.bluePrimary,
+        shape: RoundedRectangleBorder(
+          borderRadius: Helper.getRadius(20),
+          side: BorderSide(
+              color: isSecondary
+                  ? AppTheme.bluePrimary
+                  : Colors.transparent), // Add outline for secondary button
+        ),
+        minimumSize: const Size(120, 0),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        elevation: 0,
+      ),
+      child: Ktext(
+        text: text,
+        fontFamily: 'Puritan',
+        fontSize: 16,
+        color: isSecondary ? AppTheme.bluePrimary : Colors.white,
+        fontWeight: FontWeight.w700,
+      ),
+    );
+  }
+}
